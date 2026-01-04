62a80fc50723c77292331bbd1e6928b04fc888d3
Lakshay Gupta <56173382+Lakshay983@users.noreply.github.com>
2026-01-03 20:31:34 -0600

    test usb camera (#2)

---
+++ fpga_software/A53/tests/test_usb_camera.py
@@ -0,0 +1,10 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    cv2.imshow("Ultra96 USB Camera", frame)
+    if cv2.waitKey(1) == 27:
+        break
