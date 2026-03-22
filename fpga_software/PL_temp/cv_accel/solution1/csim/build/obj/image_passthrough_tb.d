efe8c90d7dce113952fb3c08ca7a82e65b40f6dc
jiwoop <jiwooo.park04@gmail.com>
2026-03-22 18:17:06 -0500

    Testing greyscale + gaussian blur

---
+++ fpga_software/PL_temp/cv_accel/solution1/csim/build/obj/image_passthrough_tb.d
@@ -0,0 +1,25 @@
+obj/image_passthrough_tb.o: ../../../../../PL/image_passthrough_tb.cpp \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/ap_axi_sdata.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/ap_int.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/ap_common.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/ap_decl.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/etc/ap_private.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/hls_half.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/hls_half_fpo.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/hls_fpo.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/floating_point_v7_1_bitacc_cmodel.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/gmp.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/mpfr.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/gmp.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/mpfr.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/ap_decl.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/hls_half_x_utils.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/ap_int_base.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/ap_int_ref.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/ap_int_special.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/ap_fixed.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/ap_fixed_base.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/ap_int.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/ap_fixed_ref.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/ap_fixed_special.h \
+ /usr/local/packages/Xilinx_2022.2/Vitis_HLS/2022.2/include/hls_stream.h
---
+++ fpga_software/PL_temp/cv_accel/solution1/script.tcl
@@ -0,0 +1,17 @@
+############################################################
+## This file is generated automatically by Vitis HLS.
+## Please DO NOT edit it.
+## Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project cv_accel
+set_top image_passthrough
+add_files ../PL/image_passthrough.cpp -cflags "-I/misc/scratch/sp02-senior-design/augmented-reality-glasses/fpga_software/PL/Vitis_Libraries/vision/L1/include -std=c++14"
+add_files -tb ../PL/image_passthrough_tb.cpp -cflags "-I/misc/scratch/sp02-senior-design/augmented-reality-glasses/fpga_software/PL/Vitis_Libraries/vision/L1/include -std=c++14"
+open_solution "solution1" -flow_target vivado
+set_part {xczu3eg-sbva484-1-i}
+create_clock -period 10 -name default
+source "./cv_accel/solution1/directives.tcl"
+csim_design -clean
+csynth_design
+cosim_design
+export_design -format ip_catalog
